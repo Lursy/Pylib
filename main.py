3c3d07e6f75d076511c9e258bf35512bf184a0d8
Lursy <70035797+Lursy@users.noreply.github.com>
2023-09-17 13:03:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
-from tools import functions, lib
+from infoFlib import Functions, Lib
+from utils.abbreviators import *
+
 from time import sleep
 
-text = functions.Text
-menu = functions.create_menu
-options = ["Cadastro", "Usuários", "Livros", "Locação", "Devolução", "Sair"]
+text = Functions.Text
+menu = Functions.Tools.create_menu
+cpf_validate = Functions.Tools.cpf_validate
 
 
-def logar(type: lib.Book) -> None:
-    # requirements recebe os valores necessários para cadastrar o livro
-    requirements = type.requirements() # [titulo, autor]
+def logar(type: Lib.Book):
+    requirements = type.requirements()
     text.clear()
     info = []
 
     print(text("Cadastro").title(style="rozzo"), end="")
 
-    # Este for percorre todos os itens necessários para o cadastro do livro
-    # salvando estes um a um na lista "info" e fazendo o cadastro logo após
-    # a inserção de todos os dados
     for item in requirements:
-        response = item[0](input(item[1]+": ")) # str(input("titulo: "))
+        response = item[0](input(item[1]+": ")) # int(input("Titulo"+": "))
         if response:
             info.append(response)
         else:
@@ -27,20 +25,28 @@ def logar(type: lib.Book) -> None:
             logar(type)
             return None
     
-    if type == lib.Book:
-        cadastro = lib.Book(titulo=info[0], autor=info[1], genero=info[2], estoque=info[3])
+    if type == Lib.Book:
+        cadastro = Lib.Book(id=info[0], titulo=info[1], autor=info[2], genero=info[3], estoque=info[4])
     else:
-        cadastro = lib.User(nome=info[0], cpf=info[1], idade=info[2])
+        if cpf_validate(info[1]):
+            cadastro = Lib.User(nome=info[0], cpf=info[1], idade=info[2])
+        else:
+            print(text("CPF inválido").red())
+            sleep(3)
+            logar(type)
+            return None
 
-    text.clear()
-    print(text("Cadastro").title(style="rozzo"), end="")
-    view: dict = cadastro.view()
+    if cadastro.registrar():
+        text.clear()
+        view: dict = cadastro.view()    
+        print(text("Cadastro").title(style="rozzo"), end="")
 
-    for key, value in view.items():
-        print(text(key + ":").yellow(), text(value).green())
+        for key, value in view.items():
+            print(text(key + ":").yellow(), text(value).green())
 
-    cadastro.registrar()
-    print(text("REGISTRADO").green())
+        print(text("REGISTRADO").green())
+    else:
+        print(text("Registro já encontrado na database").red())
     sleep(3)
 
 
@@ -56,63 +62,68 @@ def cadastro():
 
     match(choice):
         case 1:
-            logar(lib.User)
+            logar(Lib.User)
         case 2:
-            logar(lib.Book)
+            logar(Lib.Book)
         case _:
             print("Voltando...")
             sleep(2)
 
 
-def livros():
+def find(type):
+    busca = search(Lib=Lib)
+    busca = busca[type]
     text.clear()
-    print(text("acervo").title(), end="")
-    
-    acervo = lib.Book.acervo()
-    print(menu(acervo.keys()))
-    
-    input()
+    print(text(busca["title"]).title(), end="")
 
-
-def usuarios():
-    text.clear()
-    print(text("Users").title(), end="")
-
-    users = lib.User.usuarios()
+    database = busca["database"]
     
-    cpf = input("CPF: ")
-    if cpf in users.keys():
-        pessoa: dict = users[cpf]
+    choice = input(busca["input"] + ": ")
+    if choice in database.keys():
+        pessoa: dict = database[choice]
         for key, value in pessoa.items():
             print(f"{key}: {text(value).green()}")
     else:
-        print(text("Usuário não encontrado").red())
-    input()
+        print(text("não encontrado na database").red())
+    
+    input(f'\n\n{text("[i]").yellow()} - {text("tecle ENTER para voltar").green()}')
+
+
+def loc_dev(type: bool):
+    text.clear()
+    print(text("Locacao" if type else "Devolucao").title())
+    user = input("Digite o CPF do usuário: ")
+    livro = input("Digite o ID do livro: ")
+
+    valid = Lib.Loc(id=livro, cpf=user)
+    
+    if valid.locar_devo(type=type):
+        print(text("sucesso!").green())
+        sleep(3)
+    else:
+        print(text("Não foi possivel concluir a " + ("locação" if type else "devolução")).red())
+        sleep(3)
 
 
 def main():
+    options = princial(cadastro=cadastro, find=find, Lib=Lib, loc_dev=loc_dev)
+
     while True:
         text.clear()
-        title = text("Pylib").title(style="cosmic")
+        title = text("PyLib").title(style="univers")
 
         print(text(title).blue(), end="")
         print(text("Copyright © 2023 - Developed by Matheus & Paulo\n").yellow())
-        print(menu(lista=options))
-        choice = int(input(text(":// ").yellow()))
+        print(menu(lista=options.keys()))
 
-        match(choice):
-            case 1:
-                cadastro()
-            case 2:
-                usuarios()
-            case 3:
-                livros()
-            case 4:
-                pass
-            case 5:
-                pass
-            case 6:
-                break
+        choice = int(input(text(":// ").yellow()))
+        choice = list(options.keys())[choice-1]
+        item = options[choice]
+
+        if item.__class__ == tuple:
+            item[0](item[1])
+        else:
+            item()
 
 
 if __name__ == "__main__":
